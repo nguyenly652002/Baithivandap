d78154494e6ac95f3c001a46ad151b558a8ed8e6
nguyenly652002 <nguyenly652002@gmail.com>
2024-06-15 15:36:36 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenThiKhanhLy_424.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
