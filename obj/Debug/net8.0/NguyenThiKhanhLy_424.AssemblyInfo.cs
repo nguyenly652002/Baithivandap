5cb48af1db78c630c8559feaf22c0f1dbf14c3e3
nguyenly652002 <nguyenly652002@gmail.com>
2024-06-17 13:54:39 +0700

    First commit

--- obj/Debug/net8.0/NguyenThiKhanhLy_424.AssemblyInfo.cs
+++ obj/Debug/net8.0/NguyenThiKhanhLy_424.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NguyenThiKhanhLy_424")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6d8c454c9f014d889e7f0a27b2c87188a7ddf8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee7a1a546f503c873c974dc8e87856c6a1f42c18")]
 [assembly: System.Reflection.AssemblyProductAttribute("NguyenThiKhanhLy_424")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NguyenThiKhanhLy_424")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
